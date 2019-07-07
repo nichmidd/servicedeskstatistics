aaf919647cf7c4277792098cd4dc9b82cd38137b
nichmidd <nick@solutionalchemy.io>
2019-07-07 22:44:51 +1000

    lint import-stats

--- functions/import-stats/main.py
+++ functions/import-stats/main.py
@@ -1,80 +1,84 @@
+'''Need to fix - just here to make PyLint happy'''
 import sys
 import json
 import re
-from google.cloud import bigquery 
+from google.cloud import bigquery
 
 # Globals
-LocalRun = False
+LOCALRUN = False
 
-def importStats(event, context):
-    fileName = event["name"]
-    if (LocalRun):
-        print(fileName)
-    config = loadConfig()
-    importToBQ(config, fileName)
+def importstats(event, context):
+    '''Need to fix - just here to make PyLint happy'''
+    filename = event["name"]
+    if LOCALRUN:
+        print(filename)
+        print(context)
+    config = loadconfig()
+    importtobq(config, filename)
     return 200
-    
-def loadConfig():
-    with open('config.json', 'r') as c:
-        cC = json.load(c)
-    return cC
 
-def importToBQ(config, filename):
-    runType = ""
-    ra = re.compile('stats-stats', re.IGNORECASE)
-    rb = re.compile('stats-opened', re.IGNORECASE)
-    rc = re.compile('stats-closed', re.IGNORECASE)
-    if (LocalRun):
+def loadconfig():
+    '''Need to fix - just here to make PyLint happy'''
+    with open('config.json', 'r') as fileconfig:
+        configjson = json.load(fileconfig)
+    return configjson
+
+def importtobq(config, filename):
+    '''Need to fix - just here to make PyLint happy'''
+    runtype = ""
+    regexpstats = re.compile('stats-stats', re.IGNORECASE)
+    regexpopened = re.compile('stats-opened', re.IGNORECASE)
+    regexpclosed = re.compile('stats-closed', re.IGNORECASE)
+    if LOCALRUN:
         client = bigquery.Client.from_service_account_json(config["googleJSONAuthKey"])
     else:
         client = bigquery.Client()
-    if (ra.match(filename)):
-        runType = "STATS"
-        if (LocalRun):
+    if regexpstats.match(filename):
+        runtype = "STATS"
+        if LOCALRUN:
             print("Stats Import")
         dataset_id = config["Stats"]["Dataset"]
-        tableName = config["Stats"]["Table"]
-        bucketName = config["Stats"]["Bucket"]
-    elif (rb.match(filename)):
-        runType = "OPENED"
-        if (LocalRun):
+        tablename = config["Stats"]["Table"]
+        bucketname = config["Stats"]["Bucket"]
+    elif regexpopened.match(filename):
+        runtype = "OPENED"
+        if LOCALRUN:
             print("Opened Import")
         dataset_id = config["Opened"]["Dataset"]
-        tableName = config["Opened"]["Table"]
-        bucketName = config["Opened"]["Bucket"]
-    elif (rc.match(filename)):
-        runType = "CLOSED"
-        if (LocalRun):
+        tablename = config["Opened"]["Table"]
+        bucketname = config["Opened"]["Bucket"]
+    elif regexpclosed.match(filename):
+        runtype = "CLOSED"
+        if LOCALRUN:
             print("Closed Import")
         dataset_id = config["Closed"]["Dataset"]
-        tableName = config["Closed"]["Table"]
-        bucketName = config["Closed"]["Bucket"]
+        tablename = config["Closed"]["Table"]
+        bucketname = config["Closed"]["Bucket"]
     else:
         print("Error: Unable to parse filename: "+filename)
         return
     dataset_ref = client.dataset(dataset_id)
     job_config = bigquery.LoadJobConfig()
-    if (runType == "STATS"):
+    if runtype == "STATS":
         job_config.autodetect = True
-    if runType in ("OPENED","CLOSED"):
+    if runtype in ("OPENED", "CLOSED"):
         job_config.skip_leading_rows = 1
         job_config.max_bad_records = 1
-    if (runType == "OPENED"):
+    if runtype == "OPENED":
         job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
     job_config.source_format = bigquery.SourceFormat.CSV
-    uriString = "gs://" + bucketName + "/" + filename
-    load_job = client.load_table_from_uri(uriString, dataset_ref.table(tableName), job_config=job_config)
-    if (LocalRun):
+    uristring = "gs://" + bucketname + "/" + filename
+    load_job = client.load_table_from_uri(uristring, dataset_ref.table(tablename), job_config=job_config)
+    if LOCALRUN:
         print("Starting Job {}".format(load_job.job_id))
     load_job.result()
-    destTable = client.get_table(dataset_ref.table(tableName))
-    if (LocalRun):
-        print("Job Finished. Loaded {} rows".format(destTable.num_rows))
-    return
+    desttable = client.get_table(dataset_ref.table(tablename))
+    if LOCALRUN:
+        print("Job Finished. Loaded {} rows".format(desttable.num_rows))
 
 if __name__ == "__main__":
-    mockEvent = dict()
-    mockContext = dict()
-    mockEvent["name"] = sys.argv[1]
-    LocalRun = True
-    importStats(mockEvent, mockContext)
+    MOCKEVENT = dict()
+    MOCKCONTEXT = dict()
+    MOCKEVENT["name"] = sys.argv[1]
+    LOCALRUN = True
+    importstats(MOCKEVENT, MOCKCONTEXT)
