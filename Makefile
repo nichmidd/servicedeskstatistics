966385cbb48556f8afe96c33f5d91e70a3f1f563
nichmidd <nick@solutionalchemy.io>
2019-07-07 21:08:21 +1000

    lint fetch-closed

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ test:
 	echo "##TODO##"
 	#cd functions/fetch-stats && pylint main.py
 	#cd functions/fetch-open && pylint main.py
-	#cd functions/fetch-closed && pylint main.py
+	cd functions/fetch-closed && pylint main.py
 
 .PHONY: deploy
 deploy:
--- functions/fetch-closed/main.py
+++ functions/fetch-closed/main.py
@@ -1,144 +1,148 @@
+'''Need to fix - just here to make PyLint happy'''
 import sys
 import datetime
-import pytz
 import base64
-import requests
 import json
 import io
-from retrying import retry
+import requests
 from requests.utils import requote_uri
+import pytz
+from retrying import retry
 from google.cloud import storage
 
 # Global variables
-LocalRun = False
-LocalStore = False
+LOCALRUN = False
+LOCALSTORE = False
 
-def fetchClosed(event, context):
+def fetchclosed(event, context):
+    '''Need to fix - just here to make PyLint happy'''
     message = base64.b64decode(event['data']).decode('utf-8')
-    if (message == "DAILY"):
-        config = loadConfig()
-        timeSpan = calcTime()
-        getClosedTickets(timeSpan, config)
-        storeResults(timeSpan[0], config)
+    if message == "DAILY":
+        config = loadconfig()
+        timespan = calctime()
+        getclosedtickets(timespan, config)
+        storeresults(timespan[0], config)
     else:
         print("Not Supported")
+        print(context)
     return 200
 
 @retry(wait_random_min=1000, wait_random_max=5000, stop_max_attempt_number=3)
-def fetchData(urlToFetch):
-    response = requests.get(urlToFetch)
+def fetchdata(urltofetch):
+    '''Need to fix - just here to make PyLint happy'''
+    response = requests.get(urltofetch)
     return response
 
-def loadConfig():
-    with open('config.json', 'r') as c:
-        cC = json.load(c)
-    return cC
+def loadconfig():
+    '''Need to fix - just here to make PyLint happy'''
+    with open('config.json', 'r') as config:
+        configjson = json.load(config)
+    return configjson
 
-def calcTime():
+def calctime():
+    '''Need to fix - just here to make PyLint happy'''
     local_tz = pytz.timezone('UTC')
     target_tz = pytz.timezone('Australia/Sydney')
-    timeUtc = datetime.datetime.utcnow()
-    timeNow = local_tz.localize(timeUtc)
-    timeAus  = (target_tz.normalize(timeNow)).strftime("%Y-%m-%d")
-    baseStartDate = (datetime.datetime.strptime(timeAus, "%Y-%m-%d") - datetime.timedelta(days=1))
-    baseEndDate = (datetime.datetime.strptime(timeAus, "%Y-%m-%d") - datetime.timedelta(days=1))
-    tSpan = []
-    tSpan.append(str(int((datetime.datetime.combine(baseStartDate, datetime.time(0,0,0))).strftime('%s'))*1000))
-    tSpan.append(str((int((datetime.datetime.combine(baseEndDate, datetime.time(23,59,59))).strftime('%s'))*1000)+999))
-    return tSpan
+    timeutc = datetime.datetime.utcnow()
+    timenow = local_tz.localize(timeutc)
+    timeaus = (target_tz.normalize(timenow)).strftime("%Y-%m-%d")
+    basestartdate = (datetime.datetime.strptime(timeaus, "%Y-%m-%d") - datetime.timedelta(days=1))
+    baseenddate = (datetime.datetime.strptime(timeaus, "%Y-%m-%d") - datetime.timedelta(days=1))
+    tspan = []
+    tspan.append(str(int((datetime.datetime.combine(basestartdate, datetime.time(0, 0, 0))).strftime('%s'))*1000))
+    tspan.append(str((int((datetime.datetime.combine(baseenddate, datetime.time(23, 59, 59))).strftime('%s'))*1000)+999))
+    return tspan
 
-def getClosedTickets(timeSpan, config):
-    with open('queryClosed.json', 'r') as f:
-        queryJson = json.load(f)
-    queryJson["list_info"]["search_criteria"][0]["value"] = timeSpan[0]
-    queryJson["list_info"]["search_criteria"][1]["value"] = timeSpan[1]
-    baseUrl = config["url"] + '/api/v3/requests?TECHNICIAN_KEY=' + config["technicianKey"]
+def getclosedtickets(timespan, config):
+    '''Need to fix - just here to make PyLint happy'''
+    with open('queryClosed.json', 'r') as queryfile:
+        queryjson = json.load(queryfile)
+    queryjson["list_info"]["search_criteria"][0]["value"] = timespan[0]
+    queryjson["list_info"]["search_criteria"][1]["value"] = timespan[1]
+    baseurl = config["url"] + '/api/v3/requests?TECHNICIAN_KEY=' + config["technicianKey"]
     more = True
-    resultsIndex = 1
-    ticketCount = 0
-    while (more):
-        queryJson["list_info"]["start_index"] = resultsIndex
-        encodedUri = requote_uri(json.dumps(queryJson))
-        queryUrl = baseUrl + '&input_data=' + encodedUri
-        response = fetchData(queryUrl)
-        jsonResults = response.json()
-        for request in jsonResults["requests"]:
-            reqId = request["id"]
-            if (request["priority"]):
-                if (request["priority"]["name"] in config["priorityTranslations"]):
-                    reqPri = config["priorityTranslations"][request["priority"]["name"]]["Priority"]
+    resultsindex = 1
+    while more:
+        queryjson["list_info"]["start_index"] = resultsindex
+        encodeduri = requote_uri(json.dumps(queryjson))
+        queryurl = baseurl + '&input_data=' + encodeduri
+        response = fetchdata(queryurl)
+        jsonresults = response.json()
+        for request in jsonresults["requests"]:
+            reqid = request["id"]
+            if request["priority"]:
+                if request["priority"]["name"] in config["priorityTranslations"]:
+                    reqpri = config["priorityTranslations"][request["priority"]["name"]]["Priority"]
                 else:
-                    reqPri = request["priority"]["name"]
+                    reqpri = request["priority"]["name"]
             else:
-                reqPri = ""
-            if (request["site"]):
-                if (request["site"]["name"] in config["siteTranslations"]):
-                    reqSite = config["siteTranslations"][request["site"]["name"]]["Name"]
+                reqpri = ""
+            if request["site"]:
+                if request["site"]["name"] in config["siteTranslations"]:
+                    reqsite = config["siteTranslations"][request["site"]["name"]]["Name"]
                 else:
-                    reqSite = request["site"]["name"]
+                    reqsite = request["site"]["name"]
             else:
-                reqSite = "NONE"
-            reqCreated = datetime.datetime.fromtimestamp(int(int(request["created_time"]["value"])/1000)).strftime("%Y-%m-%d %H:%M:%S")
-            reqClosed = datetime.datetime.fromtimestamp(int(int(request["resolved_time"]["value"])/1000)).strftime("%Y-%m-%d %H:%M:%S")
-            resultString = reqId+","+reqPri+","+reqSite+","+reqCreated+","+reqClosed+"\n"
-            config["results"].append(resultString)
-        more = jsonResults["list_info"]["has_more_rows"]
-        if (more):
-            resultsIndex = resultsIndex + 100
-    return
+                reqsite = "NONE"
+            reqcreated = datetime.datetime.fromtimestamp(int(int(request["created_time"]["value"])/1000)).strftime("%Y-%m-%d %H:%M:%S")
+            reqclosed = datetime.datetime.fromtimestamp(int(int(request["resolved_time"]["value"])/1000)).strftime("%Y-%m-%d %H:%M:%S")
+            resultstring = reqid+","+reqpri+","+reqsite+","+reqcreated+","+reqclosed+"\n"
+            config["results"].append(resultstring)
+        more = jsonresults["list_info"]["has_more_rows"]
+        if more:
+            resultsindex = resultsindex + 100
 
-def storeResults(dayDate, config):
-    dayString = datetime.datetime.fromtimestamp(int(int(dayDate) / 1000)).strftime('%Y-%m-%d')
-    headerLine = "jobid,priority,site,opened,closed"+"\n"
-    if (LocalRun):
-        print(dayString)
-    fileName = config["storageFolder"] + dayString + ".csv"
-    if (LocalStore):
-        fileName = "./"+fileName
-        fH = open(fileName, 'w')
+def storeresults(daydate, config):
+    '''Need to fix - just here to make PyLint happy'''
+    daystring = datetime.datetime.fromtimestamp(int(int(daydate) / 1000)).strftime('%Y-%m-%d')
+    headerline = "jobid,priority,site,opened,closed"+"\n"
+    if LOCALRUN:
+        print(daystring)
+    filename = config["storageFolder"] + daystring + ".csv"
+    if LOCALSTORE:
+        filename = "./"+filename
+        filehandle = open(filename, 'w')
     else:
-        if (LocalRun):
+        if LOCALRUN:
             storage_client = storage.Client.from_service_account_json(config["googleJSONAuthKey"])
         else:
             storage_client = storage.Client()
         bucket = storage_client.get_bucket(config["googleStorageBucket"])
-        blob = bucket.blob(fileName)
-        fH = io.StringIO()
-    fH.write(headerLine)
-    for k in config["results"]:
-        fH.write(k)
-    if (LocalStore):
-        fH.close()
+        blob = bucket.blob(filename)
+        filehandle = io.StringIO()
+    filehandle.write(headerline)
+    for resultskey in config["results"]:
+        filehandle.write(resultskey)
+    if LOCALSTORE:
+        filehandle.close()
     else:
-        blob.upload_from_file(fH,rewind=True,content_type='text/csv')
-        fH.close()    
-    return
+        blob.upload_from_file(filehandle, rewind=True, content_type='text/csv')
+        filehandle.close()
 
-def calcTimeManualInput(manualTimes):
-    target_tz = pytz.timezone('Australia/Sydney')
-    startTime = datetime.datetime.strptime(str(manualTimes[0]), "%Y-%m-%d")
-    startTime = datetime.datetime.combine(startTime, datetime.time(0,0,0))
-    finTime = datetime.datetime.strptime(str(manualTimes[1]), "%Y-%m-%d")
-    finTime = datetime.datetime.combine(finTime, datetime.time(23,59,59))
-    eTime = "0"
-    fTime = str((int(finTime.strftime('%s')) * 1000) + 999)
-    while (int(fTime) > int(eTime)):
-        config = loadConfig()
-        sTime = str(int(startTime.strftime('%s')) * 1000)
-        endTime = datetime.datetime.combine(startTime, datetime.time(23,59,59))
-        eTime = str((int(endTime.strftime('%s'))*1000)+999)
-        tSpan = [sTime, eTime]
-        getClosedTickets(tSpan, config)
-        storeResults(tSpan[0], config)
-        startTime = startTime + datetime.timedelta(days=1)
-    return
+def calctimemanualinput(manualtimes):
+    '''Need to fix - just here to make PyLint happy'''
+    starttime = datetime.datetime.strptime(str(manualtimes[0]), "%Y-%m-%d")
+    starttime = datetime.datetime.combine(starttime, datetime.time(0, 0, 0))
+    fintime = datetime.datetime.strptime(str(manualtimes[1]), "%Y-%m-%d")
+    fintime = datetime.datetime.combine(fintime, datetime.time(23, 59, 59))
+    etime = "0"
+    ftime = str((int(fintime.strftime('%s')) * 1000) + 999)
+    while int(ftime) > int(etime):
+        config = loadconfig()
+        stime = str(int(starttime.strftime('%s')) * 1000)
+        endtime = datetime.datetime.combine(starttime, datetime.time(23, 59, 59))
+        etime = str((int(endtime.strftime('%s'))*1000)+999)
+        tspan = [stime, etime]
+        getclosedtickets(tspan, config)
+        storeresults(tspan[0], config)
+        starttime = starttime + datetime.timedelta(days=1)
 
 if __name__ == "__main__":
-    datesToProcess = []
-    datesToProcess.append(sys.argv[1])
-    datesToProcess.append(sys.argv[2])
-    if (len(sys.argv) > 3):
-        if (sys.argv[3] == "local"):
-            LocalStore = True
-    LocalRun = True
-    calcTimeManualInput(datesToProcess)
+    DATES_TO_PROCESS = []
+    DATES_TO_PROCESS.append(sys.argv[1])
+    DATES_TO_PROCESS.append(sys.argv[2])
+    if len(sys.argv) > 3:
+        if sys.argv[3] == "local":
+            LOCALSTORE = True
+    LOCALRUN = True
+    calctimemanualinput(DATES_TO_PROCESS)
